4a9de79b4ce0f80a7ea4d5fae2d7508708992f8d
Kajal15Shaw <kajalshaw28b@gmail.com>
2025-08-09 11:16:14 +0530

    Deploy-ready backend and frontend setup

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ import path from 'path'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: '../backend/frontend', // ← builds into the backend folder
+    outDir: 'dist', // ← builds into the backend folder
     emptyOutDir: true
   },
   resolve: {
